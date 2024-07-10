dfef0df94ce80ba3f0b99caba54bb6564e43fa0b
Aditya <aditya.salunkh919@gmail.com>
2024-07-10 20:33:45 +0530

    Changed default values and html plot for reference
    Signed-off-by: Aditya <aditya.salunkh919@gmail.com>

--- main.go
+++ main.go
@@ -11,13 +11,13 @@ import (
 func main() {
 	hfs := flag.NewFlagSet("yamcha", flag.ExitOnError)
 
-	url := hfs.String("url", " ", "Site where you want to attack")
-	numReq := hfs.Int("requests", 10, "Number of requests to send")
-	attacktype := hfs.String("attack", "basic", "type of attack")
-	plot := hfs.Bool("plot", false, "Do ya wanna plot da test as a timeseries ?")
+	url := hfs.String("url", "http://localhost:8080", "Site where you want to attack")
+	numReq := hfs.Int("requests", 110, "Number of requests to send")
+	attacktype := hfs.String("attack", "steady", "type of attack")
+	plot := hfs.Bool("plot", true, "Do ya wanna plot da test as a timeseries ?")
 	numCPUS := hfs.Int("cpus", runtime.NumCPU(), "Number of CPUs to use")
 	method := hfs.String("method", "GET", "HTTP method to use (GET, POST, etc.)")
-	rate := hfs.Int("rate", 1, "Number of requests per second")
+	rate := hfs.Int("rate", 20, "Number of requests per second")
 	burst := hfs.Int("burst", 5, "Number of bursts for burst load attack")
 	stepSize := hfs.Int("stepsize", 10, "step size for ramp up load")
 	spikeInterval := hfs.Int("spikeInterval", 10, "spike interval")
